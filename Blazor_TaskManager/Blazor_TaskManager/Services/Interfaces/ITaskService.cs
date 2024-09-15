3caf4679d545beb118d844259400d25ee7a2b72c
KhageshorGiri <girikhageshor2432@gmail.com>
2024-09-15 13:12:28 +0545

    Added Service Defination and Implementtion for taskitem

---
+++ Blazor_TaskManager/Blazor_TaskManager/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using Blazor_TaskManager.Entities;
+
+namespace Blazor_TaskManager.Services.Interfaces;
+
+public interface ITaskService
+{
+    Task<IEnumerable<TaskItem>> GetAllTaskItemsAsync();
+    Task<TaskItem> GetTaskItemByIdAsync(int id);
+    Task AddTaskItemAsync(TaskItem task);
+    Task<TaskItem> UpdateTaskItemAsync(TaskItem task);
+    Task DeleteTaskItemAsync(int id);
+}
---
+++ Blazor_TaskManager/Blazor_TaskManager/Services/TaskService.cs
@@ -0,0 +1,79 @@
+﻿using Blazor_TaskManager.Data;
+using Blazor_TaskManager.Entities;
+using Blazor_TaskManager.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_TaskManager.Services;
+
+public class TaskService : ITaskService
+{
+    private readonly TaskManagerDbContext _dbContext;
+    public TaskService(TaskManagerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddTaskItemAsync(TaskItem task)
+    {
+        using (var transaction = await _dbContext.Database.BeginTransactionAsync())
+        {
+            try
+            {
+                _dbContext.TaskItems.Add(task);
+                await _dbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+            }
+        }
+    }
+
+    public async Task DeleteTaskItemAsync(int id)
+    {
+        using (var transaction = await _dbContext.Database.BeginTransactionAsync())
+        {
+            try
+            {
+                var existingItem = await _dbContext.TaskItems.FirstOrDefaultAsync(c => c.Id == id);
+                _dbContext.TaskItems.Remove(existingItem);
+                await _dbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+            }
+        }
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetAllTaskItemsAsync()
+    {
+        return await _dbContext.TaskItems.ToListAsync();
+    }
+
+    public async Task<TaskItem> GetTaskItemByIdAsync(int id)
+    {
+        return await _dbContext.TaskItems.FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<TaskItem> UpdateTaskItemAsync(TaskItem task)
+    {
+        using (var transaction = await _dbContext.Database.BeginTransactionAsync())
+        {
+            try
+            {
+                _dbContext.TaskItems.Update(task);
+                await _dbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+            }
+
+            return task;
+        }
+    }
+}
